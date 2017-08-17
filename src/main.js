89f3d6692875b47b6ca31342eee2e2ad08d916e2
wangxingkang <wang_xingkang@qq.com>
2017-08-17 19:52:22 +0800

    补充误删文件

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import promise from 'es6-promise';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import store from './store';
+import ElementUI from 'element-ui'; // UI库
+import VueI18n from 'vue-i18n'; // 多语言库
+import 'element-ui/lib/theme-default/index.css';
+import 'normalize.css';
+import './styles/index.scss';
+
+promise.polyfill();
+
+Vue.use(ElementUI);
+Vue.use(VueI18n);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
