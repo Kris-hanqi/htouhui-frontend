bb6299972528a595e4102e06eec560cda089fef4
wangxingkang <wang_xingkang@qq.com>
2017-10-21 16:10:21 +0800

    添加字体图标，添加element-ui

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
 import 'normalize.css/normalize.css';
+import 'element-ui/lib/theme-default/index.css';
+import { Button, Select, Table, TableColumn, Pagination } from 'element-ui'
 import '@/styles/base.scss';
 import router from '../../router/home';
 import App from './App';
+
+Vue.use(Button);
+Vue.use(Select);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Pagination);
 
 new Vue({
   el: '#app',
