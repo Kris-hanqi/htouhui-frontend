be088ca1eb60687ebae9feccbfa99f70005c4d6f
wangxingkang <wang_xingkang@qq.com>
2017-10-23 20:03:57 +0800

    修改后端端口配置

--- src/utils/server-api.js
+++ src/utils/server-api.js
@@ -1,6 +1,5 @@
 // 后端地址配置数据
 const ApiAddress = {
-  mock: 'https://api-dev',
   development: '192.168.0.84:21000', // 开发环境
   test: '120.27.165.213:5001', // 测试环境
   pretest: '47.93.119.31:16666',  // 预发环境
@@ -12,11 +11,7 @@ export default function() {
   const ip = location.hostname;
   let serverAddress = null;
 
-  // 开发
-  if (ip === 'localhost') {
-    serverAddress = ApiAddress.mock;
-    return serverAddress;
-  } else if (ip === '127.0.0.1') {
+  if (ip === 'localhost' || ip === '127.0.0.1') {
     serverAddress = ApiAddress.development;
   }
 
