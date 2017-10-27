7a435cf9290dfa5b1681ea3424b7bd3a1423821b
wangxingkang <wang_xingkang@qq.com>
2017-10-27 16:05:50 +0800

    状态添加是否显示新手计划

--- src/views/home/loan-recording/index.vue
+++ src/views/home/loan-recording/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>借款记录</div>
+  <el-tabs v-model="activeName">
+    <el-tab-pane label="快捷充值" name="one">
+      121212
+    </el-tab-pane>
+    <el-tab-pane label="跨行转账" name="two">
+      12122
+    </el-tab-pane>
+    <el-tab-pane label="支付宝转账" name="there">
+      12333
+    </el-tab-pane>
+  </el-tabs>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeName: 'one'
+      }
+    }
+  }
+</script>
