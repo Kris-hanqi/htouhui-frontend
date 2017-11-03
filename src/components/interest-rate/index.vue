1acfc3f199eaa8ac7c4b3de941d76e29f404f09e
wangxingkang <wang_xingkang@qq.com>
2017-11-03 12:16:19 +0800

    利率组件提交

---
+++ src/components/interest-rate/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="interest-rate">
+    <span :style="{ fontSize: leftFontSize + 'px' }">
+      {{ leftValue }}<i :style="{ fontSize: rightFontSize + 'px' }">{{ '.' + rightValue }}</i>
+    </span>
+  </span>
+</template>
+
+<script>
+  export default {
+    props: ['value', 'leftFontSize', 'rightFontSize'],
+    computed: {
+      leftValue() {
+        return this.value.toString().split('.')[0];
+      },
+      rightValue() {
+        return this.value.toString().split('.')[1];
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .interest-rate {
+    color: #ff4a33;
+  }
+</style>
