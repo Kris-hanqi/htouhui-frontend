5e193dffbe17eb5f176e65817143bdb855c61a92
wangxingkang <wang_xingkang@qq.com>
2017-11-13 13:53:53 +0800

    关闭注册入口

--- src/components/header/header-top/index.vue
+++ src/components/header/header-top/index.vue
@@ -6,7 +6,7 @@
     <ul class="header-top_bar">
       <li><a href="./login.html">登录</a></li>
       <li><a @click.stop="logout">退出</a></li>
-      <li><a href="/memberRegist">注册</a></li>
+      <!--<li><a href="./memberRegist">注册</a></li>-->
       <li><a href="/activity/information-discloture">信息披露</a></li>
       <li><a href="/education_column_list">网贷课堂</a></li>
       <li><a href="/latestActivityList">最新活动</a></li>
