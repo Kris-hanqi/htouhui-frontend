0874af219a4fd6a5aac76a52e5918974b6867259
haitao.xiao <haitao.xiao@htouhui.com>
2017-11-15 14:06:27 +0800

    提交pages

--- src/common/Panel/index.vue
+++ src/common/Panel/index.vue
@@ -27,14 +27,14 @@
     padding: 20px;
     background-color: #fff;
     box-shadow: 0 2px 6px 0 rgba(67, 135, 186, 0.14);
-    
+
     h1 {
       font-size: 20px;
       line-height: 1;
       color: #274161;
       margin-left: 7px;
     }
-    
+
     .panel-body {
       padding-bottom: 10px;
     }
