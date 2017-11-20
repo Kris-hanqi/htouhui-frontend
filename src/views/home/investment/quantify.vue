eed9fe3ef130bc432bf46287c796a37fc1d0dc8e
hanqi <qi.han@htouhui.com>
2017-11-20 14:22:01 +0800

    【web个人中心改版】我的投资-升薪宝量化，缺少一键加入按钮（已加入用户展示界面）

--- src/views/home/investment/quantify.vue
+++ src/views/home/investment/quantify.vue
@@ -25,8 +25,8 @@
           <p class="money"><span class="roboto-regular">{{ str.raisingMoney | currency('') }}</span>元</p>
           <p>当前剩余金额</p>
         </div>
-        <a class="btn-join" href="javascript:void(0)" @click="oneKeyJoin(str.planId)" v-if="!str.joinPlan">一键加入</a>
-        <a class="btn-out" href="javascript:void(0)" @click="pullOut(str.planId)" v-else>申请退出</a>
+        <a class="btn-join" href="javascript:void(0)" @click="oneKeyJoin(str.planId)">一键加入</a>
+        <a class="btn-out" href="javascript:void(0)" @click="pullOut(str.planId)" v-if="str.joinPlan">申请退出</a>
       </div>
       <div class="shengxinbaolianghua-bottom" v-if="str.joinPlan">
         <p>在投金额（元）<span class="roboto-regular">{{ str.investMoney | currency('') }}</span></p>
@@ -41,7 +41,7 @@
   import { quantifyList } from 'api/home/quantify';
   import interestRate from 'components/interest-rate';
   import { getLocationUrl } from 'utils/index';
-  
+
   export default {
     components: {
       interestRate
@@ -89,17 +89,17 @@
     background-color: #fff;
     box-shadow: 0 2px 6px 0 rgba(67, 135, 186, 0.14);
   }
-  
+
   .title-box {
     width: 100%;
     margin-bottom: 50px;
-    
+
     .title {
       font-size: 20px;
       color: #274161;
       margin-right: 25px;
     }
-    
+
     p {
       display: inline-block;
       margin-right: 8px;
@@ -110,18 +110,18 @@
       font-size: 14px;
       color: #727e90;
     }
-    
+
     .firstDay {
       border: solid 1px #2281f2;
       color: #0e76f1;
     }
-    
+
     .tradingParticulars-1 {
       float: right;
       font-size: 14px;
       color: #727e90;
       cursor: no-drop;
-      
+
       i {
         display: inline-block;
         vertical-align: middle;
@@ -131,12 +131,12 @@
         background: url(../../../assets/images/home/center-ico-019.png) no-repeat center;
       }
     }
-    
+
     .tradingParticulars-2 {
       float: right;
       font-size: 14px;
       color: #0573f4;
-      
+
       i {
         display: inline-block;
         vertical-align: middle;
@@ -147,49 +147,49 @@
       }
     }
   }
-  
+
   .shengxinbaolianghua-main {
     width: 100%;
     text-align: center;
     margin-bottom: 45px;
-    
+
     > div {
       display: inline-block;
       width: 25%;
-      
+
       p {
         font-size: 14px;
         color: #727e90;
       }
     }
-    
+
     .shengxinbaolianghua-rate {
       .rate {
         font-size: 18px;
         color: #ff4a33;
-        
+
         span {
           font-size: 36px;
         }
-        
+
         .small-shengxinbaolianghua-rate {
           font-size: 20px;
         }
       }
     }
-    
+
     .shengxinbaolianghua-money {
       .money {
         font-size: 20px;
         font-weight: 300;
         color: #394b67;
-        
+
         span {
           font-size: 36px;
         }
       }
     }
-    
+
     .btn-join {
       float: right;
       vertical-align: top;
@@ -203,20 +203,19 @@
       text-align: center;
       font-size: 18px;
       color: #0573f4;
-      
+
       &:hover {
         background-color: #378ff6;
         color: #fff;
       }
     }
-    
+
     .btn-out {
       float: right;
       vertical-align: top;
       width: 122px;
       height: 34px;
       box-sizing: border-box;
-      margin-top: 15px;
       border-radius: 41px;
       border: solid 1px #7c86a2;
       line-height: 34px;
@@ -225,25 +224,25 @@
       color: #7c86a2;
     }
   }
-  
+
   .shengxinbaolianghua-bottom {
     width: 100%;
     height: auto;
     padding-top: 20px;
     border-top: 1px solid #dde8f3;
-    
+
     p {
       display: inline-block;
       font-size: 14px;
       color: #727e90;
       margin-right: 80px;
-      
+
       span {
         font-size: 20px;
         color: #394b67;
       }
     }
-    
+
     .seeBiao {
       float: right;
       width: 125px;
