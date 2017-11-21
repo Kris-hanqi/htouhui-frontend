c711deab0d8fb446f9f60dfb16bcd46fb69a70bc
wangxingkang <wang_xingkang@qq.com>
2017-11-22 00:59:45 +0800

    #208

--- src/views/home/loan/components/LoanRecordWaiting.vue
+++ src/views/home/loan/components/LoanRecordWaiting.vue
@@ -2,11 +2,7 @@
   <el-table :data="list" :border="false" style="width: 100%">
     <el-table-column prop="name" label="项目名称" width="150"></el-table-column>
     <el-table-column prop="loanMoney" label="借款金额" width="110"></el-table-column>
-    <el-table-column prop="predictInterst" label="预计利息" width="110">
-      <template slot-scope="scope">
-        {{ scope.row.predictInterst + '%' }}
-      </template>
-    </el-table-column>
+    <el-table-column prop="predictInterst" label="预计利息" width="110"></el-table-column>
     <el-table-column label="年利率" width="110">
       <template slot-scope="scope">
         {{ scope.row.yearRate + '%' }}
