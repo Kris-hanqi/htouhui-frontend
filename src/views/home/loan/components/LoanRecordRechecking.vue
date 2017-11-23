3642a3aca5655f2d3c06936596ff1f1e1de643a0
hanqi <qi.han@htouhui.com>
2017-11-23 15:43:20 +0800

    【web个人中心改版】借款记录-待放款-不应有“下次还款数”字段

--- src/views/home/loan/components/LoanRecordRechecking.vue
+++ src/views/home/loan/components/LoanRecordRechecking.vue
@@ -1,25 +1,24 @@
 <template>
   <el-table :data="list" :border="false" style="width: 100%">
-    <el-table-column prop="name" label="项目名称" width="100">
+    <el-table-column prop="name" label="项目名称" width="140">
       <template slot-scope="scope">
         <a :href="base + '/loan/' + scope.row.id" target="_blank">{{ scope.row.name }}</a>
       </template>
     </el-table-column>
-    <el-table-column prop="loanMoney" label="借款金额" width="100"></el-table-column>
-    <el-table-column prop="realRaisedMoney" label="实际募集金额" width="100"></el-table-column>
+    <el-table-column prop="loanMoney" label="借款金额" width="130"></el-table-column>
+    <el-table-column prop="realRaisedMoney" label="实际募集金额" width="130"></el-table-column>
     <el-table-column prop="predictInterst" label="预计利息" width="110"></el-table-column>
     <el-table-column label="年利率" width="70">
       <template slot-scope="scope">
         {{ scope.row.yearRate + '%' }}
       </template>
     </el-table-column>
-    <el-table-column label="借款期限" width="90">
+    <el-table-column label="借款期限" width="100">
       <template slot-scope="scope">
         {{ scope.row.deadline + '月' }}
       </template>
     </el-table-column>
-    <el-table-column prop="nextRepayMoney" label="下次还款数" width="100"></el-table-column>
-    <el-table-column prop="status" label="状态" width="100"></el-table-column>
+    <el-table-column prop="status" label="状态" width="110"></el-table-column>
   </el-table>
 </template>
 
