3a59abd71e7c979163a9e2af599e32b90c50b36d
wangxingkang <wang_xingkang@qq.com>
2017-12-02 16:01:40 +0800

    测试

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,9 @@ import { Router } from 'express'
 
 import users from './users'
 
-const router = Router()
+const router = Router();
 
 // Add USERS Routes
-router.use(users)
+router.use(users);
 
 export default router
