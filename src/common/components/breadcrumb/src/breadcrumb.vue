7c780ecffa8c2e31e0afb5423423c8b1753bbb3e
wangxingkang <wang_xingkang@qq.com>
2017-12-04 00:13:09 +0800

    面包屑组件开发

--- src/common/components/breadcrumb/src/breadcrumb.vue
+++ src/common/components/breadcrumb/src/breadcrumb.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>
-    面包屑导航
+  <!-- 面包屑组件 -->
+  <div class="ku-breadcrumb" aria-label="Breadcrumb" role="navigation">
+    <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
     name: 'KuBreadcrumb',
+    props: {
+      separator: {
+        type: String,
+        default: '>'
+      },
+      separatorClass: {
+        type: String,
+        default: ''
+      }
+    },
+    provide() { // 向子组件返回一个父组件实例
+      return {
+        kuBreadcrumb: this
+      };
+    },
+    mounted() {
+      const items = this.$el.querySelectorAll('.ku-breadcrumb__item');
+      if (items.length) {
+        items[items.length - 1].setAttribute('aria-current', 'page');
+      }
+    }
   }
 </script>
