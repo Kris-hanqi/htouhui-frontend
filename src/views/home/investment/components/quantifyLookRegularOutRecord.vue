61fc3d5304d4be9152e82034fcff9b1265e40a1f
hanqi <qi.han@htouhui.com>
2017-12-05 16:04:36 +0800

    【web个人中心改版】升薪宝量化，退出记录--债权信息，点击项目编号，没有跳转到项目详情页

--- src/views/home/investment/components/quantifyLookRegularOutRecord.vue
+++ src/views/home/investment/components/quantifyLookRegularOutRecord.vue
@@ -28,7 +28,11 @@
         <p class="title-message">目前已为您成功退出   <span>{{ outPlanList.exitedMoney | currency('') }}元</span></p>
       </div>
       <el-table :data="list" style="width: 100%">
-        <el-table-column prop="loanId" label="项目编号" width="140"></el-table-column>
+        <el-table-column prop="loanId" label="项目编号" width="140">
+          <template slot-scope="scope">
+            <a :href="scope.row.loanTargetUrl" target="_blank">{{ scope.row.loanId }}</a>
+          </template>
+        </el-table-column>
         <el-table-column prop="loanMoney" label="借款金额" width="120">
           <template slot-scope="scope">
             {{ scope.row.loanMoney | currency('') + '元' }}
