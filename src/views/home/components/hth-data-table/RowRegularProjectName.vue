c4c247af822cac307537a02a2283061619707647
wangxingkang <wang_xingkang@qq.com>
2017-12-05 22:19:28 +0800

    #477

--- src/views/home/components/hth-data-table/RowRegularProjectName.vue
+++ src/views/home/components/hth-data-table/RowRegularProjectName.vue
@@ -1,7 +1,7 @@
 <template>
   <el-table-column width="150" :show-overflow-tooltip="true" :label="colConfig.label">
     <span slot-scope="scope">
-      <a :href="baseUrl + '/loan/' + scope.row.investId" style="color: #409eff;" target="_blank">{{ scope.row.projectName + ' ' + scope.row.projectNameId }}</a>
+      <a :href="baseUrl + '/loan/' + scope.row.projectNameId" style="color: #409eff;" target="_blank">{{ scope.row.projectName + ' ' + scope.row.projectNameId }}</a>
     </span>
   </el-table-column>
 </template>
