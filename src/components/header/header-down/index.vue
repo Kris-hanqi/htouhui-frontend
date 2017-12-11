13c8aca12b081ab229314c25a64b8827fca10635
wangxingkang <wang_xingkang@qq.com>
2017-12-11 15:08:59 +0800

    #526

--- src/components/header/header-down/index.vue
+++ src/components/header/header-down/index.vue
@@ -10,7 +10,7 @@
         <li><a href="/shengxinbaos">升薪宝定期</a></li>
         <li><a href="/normalLoans">定期抵押</a></li>
         <li><a href="/st_page/safe">风控措施</a></li>
-        <li><a href="">关于我们</a></li>
+        <li><a href="/about/aboutus">关于我们</a></li>
         <li class="header-down__account">
           <a href="./home.html">
             <avatar :src="headImg" icon="icon-avatar"></avatar>
