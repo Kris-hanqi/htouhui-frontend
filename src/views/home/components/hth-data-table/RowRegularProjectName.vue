18b3dc41c786e3d12d074b3524a1715736f54058
wangxingkang <wang_xingkang@qq.com>
2017-12-14 16:13:08 +0800

    #432

--- src/views/home/components/hth-data-table/RowRegularProjectName.vue
+++ src/views/home/components/hth-data-table/RowRegularProjectName.vue
@@ -1,8 +1,9 @@
 <template>
   <el-table-column width="150" :show-overflow-tooltip="true" :label="colConfig.label">
-    <span slot-scope="scope">
-      <a :href="baseUrl + '/loan/' + scope.row.loanId" style="color: #409eff;" target="_blank">{{ scope.row.projectName + ' ' + scope.row.projectNameId }}</a>
-    </span>
+    <template slot-scope="scope">
+      <a :href="baseUrl + '/loan/' + scope.row.loanId" v-if="!scope.row.projectNameId" style="color: #409eff;" target="_blank">{{ scope.row.projectName }}</a>
+      <a :href="baseUrl + '/loan/' + scope.row.loanId" v-else="" style="color: #409eff;" target="_blank">{{ scope.row.projectName + '--' + scope.row.projectNameId }}</a>
+    </template>
   </el-table-column>
 </template>
 
