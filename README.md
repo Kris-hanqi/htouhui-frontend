5a70ded7866aab49a7a3dee4adeabcd14531dca6
wangxingkang <wang_xingkang@qq.com>
2018-01-22 11:50:30 +0800

    init

--- README.md
+++ README.md
@@ -1,27 +1,9 @@
-# test4
+# hth-frontend
 
-> Nuxt.js project
+> 海投汇官网项目（PC）
 
-## Build Setup
+## 开发环境搭建
 
-``` bash
-# install dependencies
-$ npm install # Or yarn install*[see note below]
-
-# serve with hot reload at localhost:3000
-$ npm run dev
-
-# build for production and launch server
-$ npm run build
-$ npm start
-
-# generate static project
-$ npm run generate
-```
-
-*Note: Due to a bug in yarn's engine version detection code if you are
-using a prerelease version of Node (i.e. v7.6.0-rc.1) you will need to either:
-  1. Use `npm install`
-  2. Run `yarn` with a standard release of Node and then switch back
-
-For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
+1. 安装NodeJS(请安装v8.0.0以上版本)
+2. 安装Yarn(安装教程请查阅[yarn](https://yarnpkg.com/zh-Hans/))
+3. 开发(命令行执行`yarn run dev || npm run dev`)
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,24 +1,41 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
+  /** 页面header */
   head: {
-    title: 'starter',
+    title: '海投汇-互联网金融平台-小额投资',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' },
+      { name: 'keywords', content: '海投汇，投资理财，互联网理财，小额投资，小额理财，网络投资' },
+      { hid: 'description', name: 'description', content: '【海投汇】真实透明，尽职风控的互联网金融理财平台；小额投资理财、短期投资、网上投资等，尽在海投汇金融服务平台。' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
-  ** Global CSS
+  ** Customize the progress bar color
   */
-  css: ['~assets/css/main.css'],
+  loading: { color: '#3B8070' },
+  // 全局样式
+  css: [
+    'normalize.css'
+  ],
   /*
-  ** Customize the progress-bar color
+  ** Build configuration
   */
-  loading: { color: '#3B8070' }
-}
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+};
